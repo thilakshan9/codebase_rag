1a7a6114c1bac8a7aa9122cbf202cf0b94731c78
Thilakshan Balasubramaniam <thilakshanbalasubramaniam9@gmail.com>
2024-12-01 22:21:40 +0000

    add: added chat bot ui using streamlit but not yet finished

--- main.py
+++ main.py
@@ -208,3 +208,37 @@ def perform_rag(query):
 response = perform_rag("How is the javascript parser used?")
 
 print(response)
+
+import streamlit as st
+
+st.title("ChatGPT-like clone")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+        
+st.session_state.messages.append({"role": "assistant", "content": response})
